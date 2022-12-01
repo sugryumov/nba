4141c07004c55c2ce479fa719fd0604857c4b773
sugryumov <snugryumov@gmail.com>
2022-12-01 20:05:18 +0600

    deploy: add base url to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import path from 'path';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/nba/',
   resolve: {
     alias: {
       // eslint-disable-next-line no-undef
